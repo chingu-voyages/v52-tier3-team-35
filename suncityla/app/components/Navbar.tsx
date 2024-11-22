1934b41312be7c03d4f61b750fe9b21fd0a62fd7
fmtabbara <fmtabbara@hotmail.co.uk>
2024-11-22 00:34:02 +0000

    new booking button in nav bar

--- suncityla/app/components/Navbar.tsx
+++ suncityla/app/components/Navbar.tsx
@@ -1,18 +1,24 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Navbar() {
-    return(
-        <nav className="flex flex-row justify-between items-center mx-9 py-9">
-            <div>
-                <Link href="/" className="text-4xl font-semibold">SunCityLA</Link>
-            </div>
-            <div className="flex flex-row justify-end gap-6 font-semibold">
-                <Link href="/">Home</Link>
-                <Link href="/about">About</Link>
-                <Link href="/bookings">Schedule an Evaluation</Link>
-                <Link href="/admins">Admin Login</Link>
-                <Link href="/Support">Support</Link>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="flex flex-row justify-between items-center mx-9 py-9">
+      <div>
+        <Link href="/" className="text-4xl font-semibold">
+          SunCityLA
+        </Link>
+      </div>
+      <div className="flex flex-row justify-end items-center gap-6 font-semibold">
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/bookings">Schedule an Evaluation</Link>
+        <Link href="/admins">Admin Login</Link>
+        <Link href="/Support">Support</Link>
+        <Button>
+          <Link href="/bookings/new">New booking</Link>
+        </Button>
+      </div>
+    </nav>
+  );
 }

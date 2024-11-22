b8cd791fafbe040bd5f3d27e3800a1a20b7d9440
fmtabbara <fmtabbara@hotmail.co.uk>
2024-11-22 00:57:56 +0000

    start moving some css over to tailwind

--- suncityla/app/components/Hero.tsx
+++ suncityla/app/components/Hero.tsx
@@ -1,15 +1,16 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Hero() {
   return (
-    <div className="relative bg-[url('/hero.jpg')] bg-cover bg-no-repeat flex items-start justify-center p-[40px_20px]">
-      <div className="overlay"></div>
-      <div className="hero-content">
+    <div className="relative bg-[url('/hero.jpg')] bg-cover bg-no-repeat flex items-start justify-center p-10">
+      <div className="absolute inset-0 bg-black opacity-70"></div>
+      <div className="hero-content relative z-10">
         <h1 className=" text-6xl text-white font-bold">
           Join Los Angeles <br />
           Move to Solar Power!
         </h1>
-        <h2 className=" text-3xl text-white font-bold mt-[33px]">
+        <h2 className=" text-3xl font-bold mt-[33px] text-yellow-300">
           We’re helping LA set up solar energy.{" "}
         </h2>
         <p className=" text-xl text-white font-medium mt-[33px] w-1/2">
@@ -17,12 +18,12 @@ export default function Hero() {
           evaluates the installation and maintenance costs of solar panels.
         </p>
         <div className="flex justify-center pt-10 ">
-          <Link
-            href="/bookings"
-            className="px-28 py-4 rounded-lg text-white bookingBtn"
+          <Button
+            size="lg"
+            className="font-bold text-white bg-blue-800 hover:bg-blue-900"
           >
-            Book Now
-          </Link>
+            <Link href="/bookings">Book Now</Link>
+          </Button>
         </div>
       </div>
     </div>

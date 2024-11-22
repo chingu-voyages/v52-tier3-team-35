ee3448a915a23f82fb734bc9ee036e3fcbaefb50
fmtabbara <fmtabbara@hotmail.co.uk>
2024-11-22 00:47:45 +0000

    reduce hero image size + move to tailwind classes

--- suncityla/app/components/Footer.tsx
+++ suncityla/app/components/Footer.tsx
@@ -1,74 +1,149 @@
 import Link from "next/link";
 
-export default function Footer(){
-    return(
-        <footer>
-            <p>v52-tier3-team-35 </p>
-            <div className=" flex flex-row justify-between gap-32 mt-4">
-                <div>
-                    <div className="devs">Developers:
-                        <div>
-                            <p>
-                                Isslem Ouederni #1: 
-                                <a href="https://github.com/isslemouederni" target="_blank" rel="noopener noreferrer">GitHub</a> / 
-                                <a href="https://linkedin.com/in/isslemouederni" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                            </p>
-                            <p>
-                                Mariana Farcas #2: 
-                                <a href="https://github.com/marianafarcas" target="_blank" rel="noopener noreferrer">GitHub</a> / 
-                                <a href="https://linkedin.com/in/marianafarcas" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                            </p>
-                            <p>
-                                Abdulsamad Yusuf: 
-                                <a href="https://github.com/abdulsamadyusuf" target="_blank" rel="noopener noreferrer">GitHub</a> / 
-                                <a href="https://linkedin.com/in/abdulsamadyusuf" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                            </p>
-                            <p>
-                                Anna Veselova #4: 
-                                <a href="https://github.com/annaveselova" target="_blank" rel="noopener noreferrer">GitHub</a> / 
-                                <a href="https://linkedin.com/in/annaveselova" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                            </p>
-                            <p>
-                                Albert Ngodi #5: 
-                                <a href="https://github.com/albertngodi" target="_blank" rel="noopener noreferrer">GitHub</a> / 
-                                <a href="https://linkedin.com/in/albertngodi" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                            </p>
-                            <p>
-                                Fouad Tabbara #3: 
-                                <a href="https://github.com/fouadtabbara" target="_blank" rel="noopener noreferrer">GitHub</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="links">
-                    <h3 className="font-bold text-lg mb-4">Useful Links</h3>
-                    <ul className="space-y-2">
-                    <li>
-                    <Link href="/" className="text-blue-500 hover:underline">
-                        Home
-                    </Link>
-                    </li>
-                    <li>
-                    <Link href="#about" className="text-blue-500 hover:underline">
-                        About
-                    </Link>
-                    </li>
-                    <li>
-                    <Link href="/bookings" className="text-blue-500 hover:underline">
-                        Book Now
-                    </Link>
-                    </li>
-                    <li>
-                    <Link href="/admins" className="text-blue-500 hover:underline">
-                        Admin Login
-                    </Link>
-                    </li>
-                </ul>
-                </div>
+export default function Footer() {
+  return (
+    <footer>
+      <p>v52-tier3-team-35 </p>
+      <div className=" flex flex-row justify-between gap-32 mt-4">
+        <div>
+          <div className="devs">
+            Developers:
+            <div>
+              <p>
+                Isslem Ouederni #1:
+                <a
+                  href="https://github.com/isslemouederni"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </a>{" "}
+                /
+                <a
+                  href="https://linkedin.com/in/isslemouederni"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LinkedIn
+                </a>
+              </p>
+              <p>
+                Mariana Farcas #2:
+                <a
+                  href="https://github.com/marianafarcas"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </a>{" "}
+                /
+                <a
+                  href="https://linkedin.com/in/marianafarcas"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LinkedIn
+                </a>
+              </p>
+              <p>
+                Abdulsamad Yusuf:
+                <a
+                  href="https://github.com/abdulsamadyusuf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </a>{" "}
+                /
+                <a
+                  href="https://linkedin.com/in/abdulsamadyusuf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LinkedIn
+                </a>
+              </p>
+              <p>
+                Anna Veselova #4:
+                <a
+                  href="https://github.com/annaveselova"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </a>{" "}
+                /
+                <a
+                  href="https://linkedin.com/in/annaveselova"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LinkedIn
+                </a>
+              </p>
+              <p>
+                Albert Ngodi #5:
+                <a
+                  href="https://github.com/albertngodi"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </a>{" "}
+                /
+                <a
+                  href="https://linkedin.com/in/albertngodi"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LinkedIn
+                </a>
+              </p>
+              <p>
+                Fouad Tabbara #3:
+                <a
+                  href="https://github.com/fouadtabbara"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </a>
+              </p>
             </div>
-            <div className="mt-4">
-                <p>Developed for chingu</p>
-            </div>
-        </footer>
-    )
+          </div>
+        </div>
+        <div className="links">
+          <h3 className="font-bold text-lg mb-4">Useful Links</h3>
+          <ul className="space-y-2">
+            <li>
+              <Link href="/" className="text-blue-500 hover:underline">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="#about" className="text-blue-500 hover:underline">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/bookings/new"
+                className="text-blue-500 hover:underline"
+              >
+                Book Now
+              </Link>
+            </li>
+            <li>
+              <Link href="/admins" className="text-blue-500 hover:underline">
+                Admin Login
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="mt-4">
+        <p>Developed for chingu</p>
+      </div>
+    </footer>
+  );
 }
--- suncityla/app/components/Hero.tsx
+++ suncityla/app/components/Hero.tsx
@@ -1,20 +1,30 @@
 import Link from "next/link";
 
-export default function Hero(){
-    return(
-        <div className="hero-bg">
-            <div className="overlay"></div>
-            <div className="hero-content" >
-                <h1 className=" text-6xl text-white font-bold">Join Los Angeles <br/>Move to Solar Power!</h1>
-                <h2 className=" text-3xl text-white font-bold mt-[33px]">We’re helping LA set up solar energy. </h2>
-                <p className=" text-xl text-white font-medium mt-[33px] w-1/2">This project provides a free service where a city-hired specialist  evaluates the installation and maintenance costs of solar panels.</p>
-                <div className="flex justify-center pt-10 ">
-                    <Link href="/bookings" className="px-28 py-4 rounded-lg text-white bookingBtn">Book Now</Link>
-                </div>
-            </div>
-
+export default function Hero() {
+  return (
+    <div className="relative bg-[url('/hero.jpg')] bg-cover bg-no-repeat flex items-start justify-center p-[40px_20px]">
+      <div className="overlay"></div>
+      <div className="hero-content">
+        <h1 className=" text-6xl text-white font-bold">
+          Join Los Angeles <br />
+          Move to Solar Power!
+        </h1>
+        <h2 className=" text-3xl text-white font-bold mt-[33px]">
+          We’re helping LA set up solar energy.{" "}
+        </h2>
+        <p className=" text-xl text-white font-medium mt-[33px] w-1/2">
+          This project provides a free service where a city-hired specialist
+          evaluates the installation and maintenance costs of solar panels.
+        </p>
+        <div className="flex justify-center pt-10 ">
+          <Link
+            href="/bookings"
+            className="px-28 py-4 rounded-lg text-white bookingBtn"
+          >
+            Book Now
+          </Link>
         </div>
-       
-    )
-
+      </div>
+    </div>
+  );
 }

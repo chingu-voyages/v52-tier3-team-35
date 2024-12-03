73178fe19d57a0e5a36345acd0419d78112fdd23
essss <sousououederni@gmail.com>
2024-12-03 21:17:18 +0200

    updated design and responsiveness

--- suncityla/components/Footer.tsx
+++ suncityla/components/Footer.tsx
@@ -1,125 +1,123 @@
 import Link from 'next/link';
 
-export default function Footer() {
+export default function Footer () {
   return (
     <footer>
       <p>v52-tier3-team-35 </p>
-      <div className=" flex flex-row justify-between gap-32 mt-4">
-        <div>
-          <div className="devs">
-            Developers:
-            <div>
-              <p>
-                Isslem Ouederni #1:
-                <a
-                  href="https://github.com/isslemouederni"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  GitHub
-                </a>{' '}
-                /
-                <a
-                  href="https://linkedin.com/in/isslemouederni"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LinkedIn
-                </a>
-              </p>
-              <p>
-                Mariana Farcas #2:
-                <a
-                  href="https://github.com/marianafarcas"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  GitHub
-                </a>{' '}
-                /
-                <a
-                  href="https://linkedin.com/in/marianafarcas"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LinkedIn
-                </a>
-              </p>
-              <p>
-                Abdulsamad Yusuf:
-                <a
-                  href="https://github.com/abdulsamadyusuf"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  GitHub
-                </a>{' '}
-                /
-                <a
-                  href="https://linkedin.com/in/abdulsamadyusuf"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LinkedIn
-                </a>
-              </p>
-              <p>
-                Anna Veselova #4:
-                <a href="https://github.com/annaveselova" target="_blank" rel="noopener noreferrer">
-                  GitHub
-                </a>{' '}
-                /
-                <a
-                  href="https://linkedin.com/in/annaveselova"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LinkedIn
-                </a>
-              </p>
-              <p>
-                Albert Ngodi #5:
-                <a href="https://github.com/albertngodi" target="_blank" rel="noopener noreferrer">
-                  GitHub
-                </a>{' '}
-                /
-                <a
-                  href="https://linkedin.com/in/albertngodi"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LinkedIn
-                </a>
-              </p>
-              <p>
-                Fouad Tabbara #3:
-                <a href="https://github.com/fouadtabbara" target="_blank" rel="noopener noreferrer">
-                  GitHub
-                </a>
-              </p>
-            </div>
+      <div className=" flex flex-row justify-between gap-10 md:gap-32 mt-4">
+        <div className="devs">
+          Developers:
+          <div>
+            <p>
+              Isslem Ouederni #1:
+              <a
+                href="https://github.com/isslemouederni"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>{' '}
+              /
+              <a
+                href="https://linkedin.com/in/isslemouederni"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
+            </p>
+            <p>
+              Mariana Farcas #2:
+              <a
+                href="https://github.com/marianafarcas"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>{' '}
+              /
+              <a
+                href="https://linkedin.com/in/marianafarcas"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
+            </p>
+            <p>
+              Abdulsamad Yusuf:
+              <a
+                href="https://github.com/abdulsamadyusuf"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>{' '}
+              /
+              <a
+                href="https://linkedin.com/in/abdulsamadyusuf"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
+            </p>
+            <p>
+              Anna Veselova #4:
+              <a href="https://github.com/annaveselova" target="_blank" rel="noopener noreferrer">
+                GitHub
+              </a>{' '}
+              /
+              <a
+                href="https://linkedin.com/in/annaveselova"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
+            </p>
+            <p>
+              Albert Ngodi #5:
+              <a href="https://github.com/albertngodi" target="_blank" rel="noopener noreferrer">
+                GitHub
+              </a>{' '}
+              /
+              <a
+                href="https://linkedin.com/in/albertngodi"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
+            </p>
+            <p>
+              Fouad Tabbara #3:
+              <a href="https://github.com/fouadtabbara" target="_blank" rel="noopener noreferrer">
+                GitHub
+              </a>
+            </p>
           </div>
         </div>
         <div className="links">
           <h3 className="font-bold text-lg mb-4">Useful Links</h3>
-          <ul className="space-y-2">
+          <ul className="space-y-2 text-[#040403]">
             <li>
-              <Link href="/" className="text-blue-500 hover:underline">
+              <Link href="/" className="hover:underline">
                 Home
               </Link>
             </li>
             <li>
-              <Link href="#about" className="text-blue-500 hover:underline">
+              <Link href="#about" className="hover:underline">
                 About
               </Link>
             </li>
             <li>
-              <Link href="/booking/new" className="text-blue-500 hover:underline">
+              <Link href="/booking/new" className="hover:underline">
                 Book Now
               </Link>
             </li>
             <li>
-              <Link href="/admin" className="text-blue-500 hover:underline">
+              <Link href="/admin" className="hover:underline">
                 Admin Login
               </Link>
             </li>
--- suncityla/components/How It Works/HowItWorks.tsx
+++ suncityla/components/How It Works/HowItWorks.tsx
@@ -1,14 +1,14 @@
 import Card from "./Card";
 
-export default function HowItWorks(){
-    return(
+export default function HowItWorks () {
+    return (
         <>
-        <h2 className="text-3xl font-semibold mb-6 text-center heading uppercase">How It Works?</h2>
-        <div className="flex justify-center items-end gap-10 mb-14">
-            <Card step="Step 1" howTo="Schedule a free evaluation online through our website" imageSrc="/step1.jpg"/>
-            <Card step="Step 2" howTo="A specialist will assess the costs and benefits for the resident’s home." imageSrc="/step2.jpg"/>
-            <Card step="Step 3" howTo="The resident receives an installation and maintenance plan." imageSrc="/step3.jpg"/>
-        </div>
+            <h2 className="text-3xl font-semibold mb-6 text-center heading uppercase">How It Works?</h2>
+            <div className="flex justify-center items-end gap-10 mb-14 flex-wrap md:flex-nowrap">
+                <Card step="Step 1" howTo="Schedule a free evaluation online through our website" imageSrc="/step1.jpg" />
+                <Card step="Step 2" howTo="A specialist will assess the costs and benefits for the resident’s home." imageSrc="/step2.jpg" />
+                <Card step="Step 3" howTo="The resident receives an installation and maintenance plan." imageSrc="/step3.jpg" />
+            </div>
         </>
     )
 }
